ebad142633ec2a4134559e212d9823a9e27cad82
JochemVH1 <jochem.vanhespen.w1504@student.hogent.be>
2017-11-13 11:13:00 +0200

    ppt added

--- app/src/main/java/com/dev/jvhis/inappbillingdemo/MainActivity.java
+++ app/src/main/java/com/dev/jvhis/inappbillingdemo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.dev.jvhis.inappbillingdemo;
 
 import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.dev.jvhis.inappbillingdemo.util.IabBroadcastReceiver;
 import com.dev.jvhis.inappbillingdemo.util.IabHelper;
@@ -12,14 +14,13 @@ import com.dev.jvhis.inappbillingdemo.util.IabResult;
 import com.dev.jvhis.inappbillingdemo.util.Inventory;
 import com.dev.jvhis.inappbillingdemo.util.Purchase;
 
-//import com.dev.jvhis.inappbillingdemo.util.IabHelper;
-
 public class MainActivity extends AppCompatActivity implements IabBroadcastReceiver.IabBroadcastListener {
 
     private IabHelper iabHelper;
     String base64 =
             "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi6NG04jSpkbJ7iQPkjUSy/bBAphCHoNyTrdKe5jIBB/R0RDohrr4L1k2eUqk6dQXOO+5hZfIEajywfYA/XhxCPHft8LHD5pXo5TcYxvkaJ0Mw3s9U7hXVvV6zA5tG6R6ZzVlA9zppx0n6uxKLxlXiyucdJdmAYEdz8AKwsVAt3BZ7nGQhPNMIiQLQSJZLrM0iBO3fr8euGxO8UFUG12HOQJG8LkrXqzS4PzkQZR7pHL3+dmBExGSY+o4zvDJ5f3PYeXCr/BGR7WDlbLECHmZATTXXfQeBxvSrgdJV7kdT2F/iqueNgQZ7VA32juWdWRRahEkGzYy/EqK3g67wV/LbwIDAQAB";
     public final String TAG = "MAIN_ACTIVITY";
+    public final int RC_REQUEST = 7777;
     private final String SKU_BUTTON_CLICKED = "button_pressed";
 
     private BroadcastReceiver mBroadcastReceiver;
@@ -111,9 +112,71 @@ public class MainActivity extends AppCompatActivity implements IabBroadcastRecei
 
     @Override
     public void receivedBroadcast() {
-
+        // Received a broadcast notification that the inventory of items has changed
+        Log.d(TAG, "Received broadcast notification. Querying inventory.");
+        try {
+            iabHelper.queryInventoryAsync(mGotInventoryListener);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            complain("Error querying inventory. Another async operation in progress.");
+        }
     }
 
+    public IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        @Override
+        public void onIabPurchaseFinished(IabResult result, Purchase info) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + info);
+
+            if (iabHelper == null) return;
+
+            if (result.isFailure()) {
+                complain("Error purchasing: " + result);
+                return;
+            }
+
+            if (info.getSku().equals(SKU_BUTTON_CLICKED)) {
+                Log.d(TAG, "Purchase is gas. Starting gas consumption.");
+                try {
+                    iabHelper.consumeAsync(info, mConsumeFinishedListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    complain("Error consuming gas. Another async operation in progress.");
+                    return;
+                }
+            }
+
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+        if (iabHelper == null) return;
+
+        // Pass on the activity result to the helper for handling
+        if (!iabHelper.handleActivityResult(requestCode, resultCode, data)) {
+            // not handled, so handle it ourselves (here's where you'd
+            // perform any handling of activity results not related to in-app
+            // billing...
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+    public void buttonClicked(View view){
+        /* TODO: for security, generate your payload here for verification. See the comments on
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use
+         *        an empty string, but on a production app you should carefully generate this. */
+        String payload = "";
+
+        try {
+            iabHelper.launchPurchaseFlow(this, SKU_BUTTON_CLICKED, RC_REQUEST,
+                    mPurchaseFinishedListener, payload);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            complain("Error launching purchase flow. Another async operation in progress.");
+            //setWaitScreen(false);
+        }
+    }
     /** Verifies the developer payload of a purchase. */
     boolean verifyDeveloperPayload(Purchase p) {
         String payload = p.getDeveloperPayload();
